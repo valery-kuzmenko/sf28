88c125666140dc6bcf95e1151c31bde9bbc72af1
Valery Kuzmenko <valerakyz@list.ru>
2016-04-14 01:07:04 +0400

    test

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($count, $firstName)
     {
-        return $this->render('YodaEventBundle:Default:index.html.twig');
+        // these 2 lines are equivalent
+        // $em = $this->container->get('doctrine')->getManager();
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('YodaEventBundle:Event');
+
+        $event = $repo->findOneBy(array(
+            'name' => 'party',
+        ));
+
+        return $this->render(
+            'YodaEventBundle:Default:index.html.twig',
+            array(
+                'name' => $firstName,
+                'count' => $count,
+                'event'=> $event,
+            )
+        );
     }
 }
